7e9b80ecf49684aaa596b6c6a8f37c422fb84474
Sakiccho <sakiccho01@gmail.com>
2017-08-11 16:01:10 +0900

    Initial Commit

---
+++ aoko/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  aoko
+//
+//  Created by YutaKanda on 2017/08/11.
+//  Copyright © 2017年 y.k. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ aoko/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  aoko
+//
+//  Created by YutaKanda on 2017/08/11.
+//  Copyright © 2017年 y.k. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
