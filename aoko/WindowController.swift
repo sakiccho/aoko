6b4f5771a1ce6e28f5ea6c2aaf3185ce36dd87c5
Sakiccho <sakiccho01@gmail.com>
2017-08-12 10:33:12 +0900

    First.

---
+++ aoko/WindowController.swift
@@ -0,0 +1,23 @@
+//
+//  WindowController.swift
+//  aoko
+//
+//  Created by YutaKanda on 2017/08/11.
+//  Copyright © 2017年 y.k. All rights reserved.
+//
+
+import Cocoa
+
+class WindowController: NSWindowController {
+
+    override func windowDidLoad() {
+        super.windowDidLoad()
+//        
+//        window?.titlebarAppearsTransparent
+//        window?.titleVisibility = .hidden
+//        window?.styleMask.insert(.fullSizeContentView)
+
+        // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+    }
+    
+}
